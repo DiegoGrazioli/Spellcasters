ef8f7456ff243f88dd990710040bcbf7df33566c
DrIce <78029654@itiscastelli.onmicrosoft.com>
2025-06-19 12:20:20 +0200

    fix: Update Vite configuration to simplify HTML input paths

--- src/spellcasters/vite.config.js
+++ src/spellcasters/vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src/spellcasters', // directory dove si trovano i tuoi HTML principali
   build: {
-    outDir: '../../dist', // dove vuoi che vada la build finale (ad esempio nella root del progetto)
+    outDir: '../../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: 'src/spellcasters/index.html',
-        home: 'src/spellcasters/home.html',
-        lab: 'src/spellcasters/lab.html',
-        'player-info': 'src/spellcasters/player-info.html',
-        'arena': 'src/spellcasters/arena.html',
-        'element-pattern-preview': 'src/spellcasters/element-pattern-preview.html'
-        // aggiungi qui altri HTML se ne hai
+        index: 'index.html',
+        home: 'home.html',
+        lab: 'lab.html',
+        'player-info': 'player-info.html',
+        arena: 'arena.html',
+        'element-pattern-preview': 'element-pattern-preview.html'
+        // aggiungi altri html se necessario
       }
     }
   }
